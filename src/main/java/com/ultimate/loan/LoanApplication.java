5ec10fa7da202c4033082f60ef158483a27ed5a9
Nikhil TK <nikhiltk1114@gmail.com>
2022-11-08 17:40:24 +0530

    changes done for automatic refresh

--- src/main/java/com/ultimate/loan/LoanApplication.java
+++ src/main/java/com/ultimate/loan/LoanApplication.java
@@ -3,11 +3,13 @@ package com.ultimate.loan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({@ComponentScan("com.ultimate.loan.controller")})
 @EnableJpaRepositories("com.ultimate.loan.repo")
 @EntityScan("com.ultimate.loan.model")
